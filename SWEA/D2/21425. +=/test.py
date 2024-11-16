bd59732ceb81ece9867e2e008ace360102f8b6e6
leechan <dlcks0601@gmail.com>
2024-11-16 23:46:43 +0900

    SWEA [D2 21425. +=]

---
+++ SWEA/D2/21425. +=/test.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+
+for test_case in range(T):
+    A, B, N = map(int, input().split())
+    count = 0
+
+    while A <= N and B <= N:
+        if A < B:
+            A += B
+        else:
+            B += A
+        count += 1
+
+    print(count)

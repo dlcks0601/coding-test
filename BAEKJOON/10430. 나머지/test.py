232c15c1fedef216eef05245d520771f5dc8dabb
leechan <dlcks0601@gmail.com>
2025-02-13 14:00:41 +0900

    10430

---
+++ BAEKJOON/10430. 나머지/test.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+A, B, C = map(int, input().split())
+
+result_1 = (A+B) % C
+result_2 = ((A % C) + (B % C)) % C
+result_3 = (A*B) % C
+result_4 = ((A % C) * (B % C)) % C
+
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)

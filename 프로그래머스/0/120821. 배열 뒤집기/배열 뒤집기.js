558b3d90e9db414e5f9f1e51f763f9f9f8e725a6
leechan <dlcks0601@gmail.com>
2025-07-02 10:56:16 +0900

    메서드 사용

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,10 +1,13 @@
 function solution(num_list) {
-    let l = num_list.length;
-    for (let i = 0; i < Math.floor(l / 2); i++) {
-        const temp = num_list[i];
-        num_list[i] = num_list[l-1-i];
-        num_list[l-1-i] = temp;
-    }
-    return num_list
+  let l = num_list.length;
+  for (let i = 0; i < Math.floor(l / 2); i++) {
+    const temp = num_list[i];
+    num_list[i] = num_list[l - 1 - i];
+    num_list[l - 1 - i] = temp;
+  }
+  return num_list;
 }
 
+// function solution(num_list) {
+//     return num_list.reverse();
+// }

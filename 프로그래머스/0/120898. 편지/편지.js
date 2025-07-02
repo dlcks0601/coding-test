b143f532d1fdb0ac9fe5cd181f11285dcd173fc6
leechan <dlcks0601@gmail.com>
2025-07-02 16:02:07 +0900

    문자열 길이 추가

--- 프로그래머스/0/120898. 편지/편지.js
+++ 프로그래머스/0/120898. 편지/편지.js
@@ -1,6 +1,10 @@
 function solution(message) {
-    // 글자 한 자를 가로 2cm, 가로로만 작성, message = 14
-    // 공백도 하나의 문자로 취급
-    let textArray= message.split("")
-    return textArray.length * 2
+  // 글자 한 자를 가로 2cm, 가로로만 작성, message = 14
+  // 공백도 하나의 문자로 취급
+  let textArray = message.split('');
+  return textArray.length * 2;
 }
+
+// function solution(message) {
+//   return message.length;
+// }

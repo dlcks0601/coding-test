e31b607ac4fdecf2ed03156e85ebee8f3bdc3a6c
leechan <dlcks0601@gmail.com>
2025-07-02 11:09:04 +0900

    메서드 사용

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,8 +1,12 @@
 function solution(n) {
-    for (let i = 1; i < n; i++) {
-        if (n / i === i) {
-            return 1;
-        }
+  for (let i = 1; i < n; i++) {
+    if (n / i === i) {
+      return 1;
     }
-    return 2;
+  }
+  return 2;
 }
+
+// function solution(n) {
+//     return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
+// }

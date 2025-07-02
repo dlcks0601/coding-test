4727360e404b7a1ed4c3997ad5891fb35f07fc12
leechan <dlcks0601@gmail.com>
2025-07-02 13:13:19 +0900

    메서드 사용

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,9 +1,17 @@
 function solution(my_string, letter) {
-    let result = "";
-    for (let i = 0; i < my_string.length; i++) {
-        if (my_string[i] !== letter) {
-            result += my_string[i];
-        }
+  let result = '';
+  for (let i = 0; i < my_string.length; i++) {
+    if (my_string[i] !== letter) {
+      result += my_string[i];
     }
-    return result;
+  }
+  return result;
 }
+
+// function solution(my_string, letter) {
+//     return my_string.replaceAll(letter, "");
+// }
+
+// function solution(my_string, letter) {
+//     return my_string.replace(new RegExp(letter, "g"), "");
+// }

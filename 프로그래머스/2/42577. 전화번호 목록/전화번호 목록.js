acba778135d648f825c3f564319ce6bff4c1f49b
leechan <51065636+dlcks0601@users.noreply.github.com>
2025-09-07 21:14:59 +0900

    [level 2] Title: 전화번호 목록, Time: 270.66 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,15 +1,12 @@
 function solution(phone_book) {
-  // 해시 테이블 생성
-  const hash = new Set(phone_book);
-
-  // 각 전화번호의 모든 가능한 접두사 확인
-  for (let phone of phone_book) {
-    for (let i = 1; i < phone.length; i++) {
-      const prefix = phone.substring(0, i);
-      if (hash.has(prefix)) {
-        return false;
-      }
+    const set = new Set(phone_book)
+    for (const number of phone_book) {
+        for (let i = 0; i<number.length; i++) {
+            const prefix = number.slice(0,i);
+            if ( set.has(prefix)) {
+                return false;
+            }
+        }
     }
-  }
-  return true;
+    return true
 }
